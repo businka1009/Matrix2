51c4cdb61c37759f9ab8cdbddfaf0cda55f31e9b
SirAristotel <igor-rus48@hotmail.com>
2015-03-09 22:40:42 +0400

    добавил фаил main

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "Matrix.hpp"
+#include "Rmax.hpp"
+#include <iostream>
+
+/*template<typename Type>
+Type min(Type a, Type b)
+{
+	if (a<b)
+		return a;
+	else return b;
+}
+*/
+
+void main()
+{
+	Matrix<Rmax> A(Rmax);
+	Matrix<Rmax> B(3,2);
+	Matrix<Rmax> C(B);
+	//int c,d;
+	//std::cin>>c>>d;
+	//std::cout<<min(c,d)<<std::endl;
+}
